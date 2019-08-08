4c56db76fcdf4ab23f2c5c6d16c03c06ca1a01f6
Matheus Machado <matheus-machadovilarino@crptecnologia.com.br>
2019-08-08 01:14:51 -0300

    Tentando fazer a migration de pontos de parada 🐎

--- src/Core/Business/DataBaseContext.cs
+++ src/Core/Business/DataBaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using VouDeVan.Core.Business.Domains.StopoverPoints;
 using VouDeVan.Core.Business.Domains.TransportationCompanies;
 
 namespace VouDeVan.Core.Business
@@ -6,10 +7,10 @@ namespace VouDeVan.Core.Business
     public class DataBaseContext : DbContext
     {
         public DbSet<TransportationCompany> TransportationCompanies { get; set; }
+        public DbSet<StopoverPoint> StopoverPoints { get; set; }
 
         public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
         {
-
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
